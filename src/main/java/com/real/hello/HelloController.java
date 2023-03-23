84aaf81f1cc66983fe622e008be39420eee6a687
venkatesh <venki@9282@gmail.com>
2023-03-23 12:30:13 +0530

    initial commit

---
+++ src/main/java/com/real/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.real.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
